f92cb074fc66b5dad77ed28159617c7541511de2
HGThinh <gia.thinh.hoang@student.ehb.be>
2024-08-19 02:06:10 +0200

    Add nieuw addon om customers informatie in api naar rabbitmq te sturen

---
+++ odoo_custom_addons/customers_to_wordpress/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Customers to WordPress',
+    'version': '1.0',
+    'category': 'Sales',
+    'summary': 'Send customers updates to WordPress via RabbitMQ',
+    'depends': ['base', 'product', 'rabbitmq_connector'],
+    'data': [],
+    'installable': True,
+    'application': False,
+}
